2cc53f8ec08f78e77be3ccc2d5c68c68138f1c92
zero060699 <builoc191999@gmail.com>
2020-11-15 16:46:53 +0700

    python

--- main.py
+++ main.py
@@ -67,36 +67,44 @@ testY = 10
 over_font = pygame.font.Font('freesansbold.ttf', 64)
 
 
-def show_score(x, y):
-    score = font.render("Score : " + str(score_value), True, (255, 255, 255))
-    screen.blit(score, (x, y))
+class show_score:
+    def __init__(x, y):
+        score = font.render("Score : " + str(score_value),
+                            True, (255, 255, 255))
+        screen.blit(score, (x, y))
 
 
-def game_over_text():
-    over_text = over_font.render("GAME OVER", True, (255, 255, 255))
-    screen.blit(over_text, (200, 250))
+class game_over_text:
+    def __init__():
+        over_text = over_font.render("GAME OVER", True, (255, 255, 255))
+        screen.blit(over_text, (200, 250))
 
 
-def player(x, y):
-    screen.blit(playerImg, (x, y))
+class player:
+    def __init__(x, y):
+        screen.blit(playerImg, (x, y))
 
 
-def enemy(x, y, i):
-    screen.blit(enemyImg[i], (x, y))
+class enemy:
+    def __init__(x, y, i):
+        screen.blit(enemyImg[i], (x, y))
 
 
-def fire_bullet(x, y):
-    global bullet_state
-    bullet_state = "fire"
-    screen.blit(bulletImg, (x + 16, y + 10))
+class fire_bullet:
+    def __init__(x, y):
+        global bullet_state
+        bullet_state = "fire"
+        screen.blit(bulletImg, (x + 16, y + 10))
 
 
-def isCollision(enemyX, enemyY, bulletX, bulletY):
-    distance = math.sqrt(math.pow(enemyX - bulletX, 2) + (math.pow(enemyY - bulletY, 2)))
-    if distance < 27:
-        return True
-    else:
-        return False
+class isCollision:
+    def __init__(enemyX, enemyY, bulletX, bulletY):
+        distance = math.sqrt(math.pow(enemyX - bulletX, 2) +
+                             (math.pow(enemyY - bulletY, 2)))
+        if distance < 27:
+            return True
+        else:
+            return False
 
 
 # Game Loop
